3a135dc63760cd64919d6622e3512bd510823a61
hkvp9 <41391262+hkvp9@users.noreply.github.com>
2018-08-20 12:55:47 -0400

    commit quiz 7

--- src/Shop.java
+++ src/Shop.java
@@ -13,12 +13,12 @@ public class Shop {
 	static Scanner sc = new Scanner(System.in);
 	Shop shop; 
 //	static  Product[] shopping = new Product[5]; 
-	static Product [] shopping = ]  { new ("Business Ritual Women's long sleeve top",
-			65.39, "Apparel", 4, 'L', "blue") , new ("The Art of Computer Programming", 190.54,
-			"Books", 9, "Donald E. Knuth") , ("Men's Business Casual Shirt", 57.10,
-			"Apparel", 2, 'M', "Teal") , ("Organic Tumeric Soap", 11.25,
-			"Bath & Beauty", 1) , ("Head First Design Pattern", 37.35, "Books",
-					3, "Eric Freeman")} ;
+	static Product [] shopping =  { new Product ("Business Ritual Women's long sleeve top", 65.39, "Apparel", 4, 'L', "blue") ,
+			new Product ("The Art of Computer Programming", 190.54, "Books", 9, "Donald E. Knuth") ,
+			new Product ("Men's Business Casual Shirt", 57.10, "Apparel", 2, 'M', "Teal") ,
+			new Product ("Organic Tumeric Soap", 11.25,"Bath & Beauty", 1) ,
+			new Product("Head First Design Pattern", 37.35, "Books", 3, "Eric Freeman") };
+	
 //
 //	shopping[0] = new Product("Business Ritual Women's long sleeve top",
 //			65.39, "Apparel", 4, 'L', "blue");
@@ -42,38 +42,49 @@ public class Shop {
 //		if (isShopOpen==true){
 //			System.out.println("Enter the product number (0 to 4) you want to buy."
 //					+ "To exit the shopping cart, enter any number other than 0 to 4");
+		if( ! isShopOpen){
+			System.out.println("Shop is not open at this time. Please come back later!");
+		}
+	
+		else{
+	while (true) {
+		System.out
+				.println("Enter the product number (0 to 4) you want to buy."
+						+ "To exit the shopping cart, enter any number other than 0 to 4");
+		for (int x = 0; x < shopping.length; x++) {
+			System.out.println(x + ":" + shopping[x].getName());
+		}
+		try {
+
+			userinput = sc.nextInt();
+			if (shopping[userinput].buy()) {
+				System.out.println("Your purchase was successful "+"Thank you for shopping with us! The total amount is "+ totalAmount);
+				totalAmount += shopping[userinput].getPrice();
+			} else {
+				System.out.println("Sorry! This product is out of stock.");
+				System.out.println("Thank you for shopping with us! The total amount is "+ totalAmount);
+				break;
+			}
+		} catch (InputMismatchException e) {
+			System.out.println("Invalid input, exiting the shopping cart." +totalAmount);
+			break;
+		}
+		catch( ArrayIndexOutOfBoundsException e){
+			System.out.println("Array index out of bounds - Total amount is "+"\n" + totalAmount );
+			break;
+		} 
+			
+	
+}
+
+
+	}
+		sc.close();
 		
 	
-	for (int x = 0; x < shopping.length; x++) {
-		System.out.println(x + ":" + shopping[x].getName());
+		}
 	}
-	try {
-		userinput = sc.nextInt();
-	} catch (InputMismatchException e) {
-		System.out.println("Invalid input, exiting the shopping cart." +totalAmount);
-//		break;
 	
-	}
-	if (userinput >= 0 && userinput <= 4) {
-		if (shopping[userinput].buy()) {
-			System.out.println("Your purchase was successful"+"Thank you for shopping with us! The total amount is "+ totalAmount);
-			totalAmount += shopping[userinput].getPrice();
-		} 
-		else if(isShopOpen= ! false){
-			System.out.println("Sorry the shop is closed!");
-			
-		}
-	else {
-				System.out.println("Thankyou for visiting!" +totalAmount);
-			}
-
-
-
-		sc.close();
-		}
-
-		}
-	}
 
 
 	
